9f05760df6e5b0cd63a1d299b2d485840b372b63
developer-nawwar <nawwarkhaddaj@gmail.com>
2022-04-25 04:39:39 +0300

    adding animation to the header arrow

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,18 @@ $(function(){
     /*trigger*/
     'use strict';
     $('html').nicescroll();
-   /*change the header height*/
+  
+ 
   
  
 });
+ /*change the header height*/
 $(".header").height($(window).height());
+  // scroll to feature
+  $("#arrow").click(function(){
+    // $(window).scrollTop($('.features').offset().top);
+    $('html,body').animate({
+        scrollTop:$('.features').offset().top
+    }, 1000);
+
+});
