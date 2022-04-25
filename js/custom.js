6e972034783f830b54def6bc848f650ae2d827f9
developer-nawwar <nawwarkhaddaj@gmail.com>
2022-04-25 03:32:49 +0300

    header height extends as the window

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,10 @@
 /*global $, alert, console */
 $(function(){
-    //trigger
+    /*trigger*/
     'use strict';
     $('html').nicescroll();
-   
-
-
-
-
-
-
-
-
+   /*change the header height*/
+  
+ 
 });
+$(".header").height($(window).height());
