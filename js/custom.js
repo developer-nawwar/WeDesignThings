b9251f9d78ddb307d2235a80ea6628a5e468145e
developer-nawwar <nawwarkhaddaj@gmail.com>
2022-04-26 03:53:12 +0300

    Slider Animation

--- js/custom.js
+++ js/custom.js
@@ -20,27 +20,41 @@ $(".header").height($(window).height());
 });
 //Shoe Hidden Items From Work 
 $('.show-more').click(function(){
-    // switch($(".our-work .hidden:visible"))
-        
-    // }
-   if ($(".our-work .hidden").is(":hidden")){
-       $(".show-more").text("show less");
+        if ($(".our-work .hidden").is(":hidden"))
+        {
+                $(".show-more").text("show less");
 
-   }
-   else{
-    $(".show-more").text("show more");
+        }
+        else
+        {
+                $(".show-more").text("show more");
+        } 
+       $(".our-work .hidden").fadeToggle(500);
+});
+//Check Testimonials
+let leftArrow=$(".testimonials .fa-chevron-left");
+let rightArrow=$(".testimonials .fa-chevron-right ");
+function checkTestimonials ()
+{
+$(".client:first").hasClass("active")? leftArrow.fadeOut() : leftArrow.fadeIn();
+$(".client:last").hasClass("active")? rightArrow.fadeOut() : rightArrow.fadeIn();
+}
+checkTestimonials();
+$(".testimonials i").click(function(){
     
-
-   }
- 
-  
-$(".our-work .hidden").fadeToggle(500);
-
-
-
+    if($(this).hasClass("fa-chevron-right")){
     
+       
+        $(".testimonials .active").fadeOut(100, function(){
+            $(this).removeClass("active").next(".client").addClass("active").fadeIn();
+            checkTestimonials();
+        });
+    }
+    else{
+        $(".testimonials .active").fadeOut(100, function(){
+            $(this).removeClass("active").prev(".client").addClass("active").fadeIn();
+            checkTestimonials();
+        });
+    }
 
-
-    
-    
-})
+});
