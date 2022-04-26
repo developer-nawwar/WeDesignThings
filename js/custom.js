91c4ff3b4a771d79fdf50c4ac14457991932101a
developer-nawwar <nawwarkhaddaj@gmail.com>
2022-04-27 02:23:40 +0300

    Code Organization JavaScript

--- js/custom.js
+++ js/custom.js
@@ -10,41 +10,36 @@ $(function(){
     }
 
     );
-  
- 
-  
- 
-});
- /*change the header height*/
+     /*change the header height*/
 $(".header").height($(window).height());
-  // scroll to feature
-  $("#arrow").click(function(){
-    // $(window).scrollTop($('.features').offset().top);
-    $('html,body').animate({
-        scrollTop:$('.features').offset().top
-    }, 1000);
+// scroll to feature
+$("#arrow").click(function(){
+  // $(window).scrollTop($('.features').offset().top);
+  $('html,body').animate({
+      scrollTop:$('.features').offset().top
+  }, 1000);
 
 });
 $(".hire").click(function(){
-   
-    
-    $('html,body').animate({
-        scrollTop: $('.our-team').offset().top
-    },2000);
+ 
+  
+  $('html,body').animate({
+      scrollTop: $('.our-team').offset().top
+  },2000);
 
 });
 //Shoe Hidden Items From Work 
 $('.show-more').click(function(){
-        if ($(".our-work .hidden").is(":hidden"))
-        {
-                $(".show-more").text("show less");
+      if ($(".our-work .hidden").is(":hidden"))
+      {
+              $(".show-more").text("show less");
 
-        }
-        else
-        {
-                $(".show-more").text("show more");
-        } 
-       $(".our-work .hidden").fadeToggle(500);
+      }
+      else
+      {
+              $(".show-more").text("show more");
+      } 
+     $(".our-work .hidden").fadeToggle(500);
 });
 //Check Testimonials
 let leftArrow=$(".testimonials .fa-chevron-left");
@@ -56,20 +51,26 @@ $(".client:last").hasClass("active")? rightArrow.fadeOut() : rightArrow.fadeIn()
 }
 checkTestimonials();
 $(".testimonials i").click(function(){
-    
-    if($(this).hasClass("fa-chevron-right")){
-    
-       
-        $(".testimonials .active").fadeOut(100, function(){
-            $(this).removeClass("active").next(".client").addClass("active").fadeIn();
-            checkTestimonials();
-        });
-    }
-    else{
-        $(".testimonials .active").fadeOut(100, function(){
-            $(this).removeClass("active").prev(".client").addClass("active").fadeIn();
-            checkTestimonials();
-        });
-    }
+  
+  if($(this).hasClass("fa-chevron-right")){
+  
+     
+      $(".testimonials .active").fadeOut(100, function(){
+          $(this).removeClass("active").next(".client").addClass("active").fadeIn();
+          checkTestimonials();
+      });
+  }
+  else{
+      $(".testimonials .active").fadeOut(100, function(){
+          $(this).removeClass("active").prev(".client").addClass("active").fadeIn();
+          checkTestimonials();
+      });
+  }
 
+});
+
+  
+ 
+  
+ 
 });
